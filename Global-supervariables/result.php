2d906d7cc6e6494c35c3adc530a544868a79a52f
Ollivdh <olivier.vandenheede.mailbox@gmail.com>
2019-05-15 14:10:03 +0200

    bonus .2

--- Global-supervariables/result.php
+++ Global-supervariables/result.php
@@ -19,27 +19,32 @@ $worst_movie = $_GET["Worst-movie"];
 
 <div class="container">
     <h3>Handling your data</h3>
-    <ul class="nav nav-tabs">
-        <li class="<?php if ($_SERVER['REQUEST_METHOD'] == 'POST') { echo "active"; }?>"><a href="#">POST</a></li>
-        <li class="<?php if ($_SERVER['REQUEST_METHOD'] == 'GET') { echo "active"; }?>"><a href="#">GET</a></li>
+    <ul class="nav nav-pills">
+        <li class="<?php if ($_SERVER['REQUEST_METHOD'] == 'POST') { echo "active"; }?>"><a data-toggle="pill" href="#post">POST</a></li>
+        <li class="<?php if ($_SERVER['REQUEST_METHOD'] == 'GET') { echo "active"; }?>"><a data-toggle="pill" href="#get">GET</a></li>
     </ul>
 
-    <div id="output">
-        <?php
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        echo "Your top 5 shows are: $top_shows";
-        echo ", and your top 5 movies are: $top_movies".".";
-    }
-    else if($_SERVER['REQUEST_METHOD'] == 'GET') {
-        echo "Your favorite country is: $fav_country";
-        echo ", nd your worst ever movie is: $worst_movie".".";
-    }
-    else{
-        return;
-    }?>
+    <div class="tab-content">
+        <?php if($_SERVER['REQUEST_METHOD'] == 'POST') : ?>
+            <div id="post" class="tab-pane fade in active">
+                <?php
+                echo "Your top 5 shows are: $top_shows";
+                echo ", and your top 5 movies are: $top_movies".".";
+                ?>
+            </div>
+        <?php elseif($_SERVER['REQUEST_METHOD'] == 'GET') : ?>
+            <div id="get" class="tab-pane fade in active">
+                <?php
+                echo "Your favorite country is: $fav_country";
+                echo ", and your worst ever movie is: $worst_movie".".";
+                ?>
+            </div>
+        <?php endif ?>
     </div>
+</div>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
--- Global-supervariables/setup.php
+++ Global-supervariables/setup.php
@@ -8,18 +8,18 @@
     <title>Setup</title>
 </head>
 <body>
-<form action="result.php" method="post" target="outputOne">
-    Top 5 tv shows:<input type="text" id="shows" name="Top-shows"/>
-    top 5 movies:<input type="text" id="topMovies" name="Top-movies"/>
-    <input type="submit" id="one" value="submit">
+<form action="result.php" method="post">
+    Top 5 tv shows:<input type="text" name="Top-shows"/>
+    top 5 movies:<input type="text" name="Top-movies"/>
+    <input type="submit" value="submit">
 </form>
 
 <br>
 
-<form action="result.php" method="get" target="outputTwo">
-    Favourite country:<input type="text" id="country" name="Favourite-country"/>
-    Worst movie ever seen:<input type="text" id="worstMovie" name="Worst-movie"/>
-    <input type="submit" id="two" value="submit">
+<form action="result.php" method="get">
+    Favourite country:<input type="text" name="Favourite-country"/>
+    Worst movie ever seen:<input type="text" name="Worst-movie"/>
+    <input type="submit" value="submit">
 </form>
 
 
